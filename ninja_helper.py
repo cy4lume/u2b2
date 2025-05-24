f211141de1dc1f94e195356ac0e4ce35ba94a863
as3617 <ohseungju5@gmail.com>
2025-05-25 05:55:10 +0900

    add ninja helper

---
+++ ninja_helper.py
@@ -0,0 +1,17 @@
+from binaryninja import *
+
+def get_got(bv: BinaryView) -> list[str]:
+    ctx = UIContext.activeContext()
+    if ctx is None:
+        return None
+    view = ctx.getCurrentView()
+    if view is None:
+        return None
+    bv = view.getData()
+    load_functions = {}
+    got_section = bv.get_section_by_name(".got")
+    if got_section:
+        for address in range(got_section.start, got_section.end, 0x4):
+            func_name = bv.get_symbol_at(address).name
+            load_functions[address] = func_name
+        return load_functions
