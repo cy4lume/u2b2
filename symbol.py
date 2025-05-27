dc936f8168aa59eb641a76d6f2a0265495775968
cy4lume <cyalumej@gmail.com>
2025-05-28 04:57:11 +0900

    impl libc mapper

--- symbol.py
+++ symbol.py
@@ -1,5 +1,29 @@
 from elftools.elf.sections import SymbolTableSection, Section
 from elftools.elf.dynamic import DynamicSection
+import ctypes.util
+from elftools.elf.elffile import ELFFile
+from unicorn import (
+    Uc,
+    UC_ARCH_MIPS,
+    UC_MODE_MIPS32,
+    UC_MODE_BIG_ENDIAN,
+    UC_PROT_READ,
+    UC_PROT_WRITE,
+    UC_PROT_EXEC,
+    UC_HOOK_CODE
+)
+import os
+
+
+PAGE_SIZE = 0x1000
+LIBC_BASE = 0x7F000000
+
+def align_down(addr, align=PAGE_SIZE):
+    return addr & ~(align - 1)
+
+
+def align_up(addr, align=PAGE_SIZE):
+    return (addr + align - 1) & ~(align - 1)
 
 
 def dlloader(elffile, pointer_size):
@@ -92,3 +116,48 @@ def get_symbol_table(elffile) -> dict[int, str]:
             else:
                 continue
     return got_function_map
+
+class LibcSym:
+    def __init__(self, uc, path):
+        libc_path = os.path.join(path, "libc.so.6")
+
+        f = open(libc_path, "rb")
+        libc = ELFFile(f)
+
+        dynsym = libc.get_section_by_name(".dynsym")
+        self.dynsym  = { sym.name: sym['st_value']
+                   for sym in dynsym.iter_symbols() if sym.name }
+
+        for seg in libc.iter_segments():
+            if seg.header.p_type != "PT_LOAD":
+                continue
+
+            vaddr = seg.header.p_vaddr
+            memsz = seg.header.p_memsz
+            filesz = seg.header.p_filesz
+            data = seg.data()
+
+            base = align_down(vaddr + LIBC_BASE)
+            top = align_up(vaddr + memsz + LIBC_BASE)
+            size = top - base
+
+            uc.mem_map(base, size,
+                        UC_PROT_READ | UC_PROT_WRITE | UC_PROT_EXEC)
+        
+            uc.mem_write(vaddr + LIBC_BASE, data)
+
+            if memsz > filesz:
+                uc.mem_write(vaddr + LIBC_BASE + filesz,
+                            b"\x00" * (memsz - filesz))
+
+            print(hex(dynsym.get_symbol_by_name('malloc')[0]['st_value']))
+            print(dynsym)
+    
+    def get(self, fn: str)->int:
+        # get libc function's vma
+        rva = self.dynsym.get(fn)
+
+        if not rva:
+            raise KeyError(f"{fn} is not an libc symbol")
+        
+        return LIBC_BASE + rva
